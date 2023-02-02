1ab5b2328b0fd41df22b3a356ede5771b8ea8652
GulYALCIN38 <gulfen3@gmail.com>
2023-02-02 11:11:59 +0100

    v1

---
+++ src/test/java/tests/PageKullanimi.java
@@ -0,0 +1,20 @@
+package tests;
+
+import org.openqa.selenium.Keys;
+import org.testng.annotations.Test;
+import pages.AmazonPage;
+import utilities.Driver;
+
+public class PageKullanimi {
+
+    @Test
+    public void test() {
+        Driver.getDriver().get("https://amazon.com");
+
+        AmazonPage amazonPage = new AmazonPage();
+        //iphon aratalim
+        amazonPage.aramaKutusu.sendKeys("iphone", Keys.ENTER);
+        //sonuc yazısının iphone içerdiğini test edelim
+        assert amazonPage.sonucYazisi.getText().contains("iphone");
+    }
+}
